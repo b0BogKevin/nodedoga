9c8ac0a6628fde332bd903a9cec39a6fbb815e22
b0bogkev <b0bogkev@vasvari.org>
2024-11-20 14:32:09 +0100

    file megjelenik

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-
+import path from "path";
 import rootpath from "../util/rootpath.js";
 
 const router = express.Router()
 
 router.get("/",(req,res)=>{
-    res.send()
+    res.sendFile(path.join(rootpath,"public","index.html"))
 })
 
 export default router
